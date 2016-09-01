549a1ddb29c94c14bf4c9743c5139a83b76fbb7e
Mauri de Souza Nunes <mauri870@gmail.com>
2016-09-01 15:55:28 -0300

    Add benchmark

--- crypto/main_test.go
+++ crypto/main_test.go
@@ -1,15 +1,16 @@
 package crypto
 
 import (
+	"strconv"
 	"testing"
 )
 
 var (
 	stubs = []string{"Hello World", "The Quick Brown Fox", "The difficult we do immediately; the impossible takes a little longer."}
+	key   = []byte("hDmPPK2b76ROBqd4uWQcu0ruUyz0tVXd")
 )
 
 func TestEncryptDecrypt(t *testing.T) {
-	key := []byte("hDmPPK2b76ROBqd4uWQcu0ruUyz0tVXd")
 	for _, text := range stubs {
 		ciphertext, err := Encrypt(key, []byte(text))
 		if err != nil {
@@ -24,5 +25,12 @@ func TestEncryptDecrypt(t *testing.T) {
 		if string(decryptedText) != text {
 			t.Errorf("Expect %s but got %s on decryption", text, decryptedText)
 		}
+	}
+}
+
+func BenchmarkEncryptDecrypt(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ciphertext, _ := Encrypt(key, []byte("bench"+strconv.Itoa(n)))
+		Decrypt(key, ciphertext)
 	}
 }
